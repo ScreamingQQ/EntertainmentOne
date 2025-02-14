075657050f1da875ae67c903609a8fa0a2a854d6
ScreamingQQ <157857659+ScreamingQQ@users.noreply.github.com>
2025-02-14 22:28:10 +0000

    Pre-developement

---
+++ EntertainmentOne/src/components/More.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const More = () => {
+    return (
+        <div>
+            <h1>More Entertainment Options</h1>
+            <p>Explore additional entertainment content including music, podcasts, and more!</p>
+        </div>
+    );
+};
+
+export default More;
